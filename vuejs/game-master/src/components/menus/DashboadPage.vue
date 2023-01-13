f588dcbfacb83e2311e2bd114b21c20fbf35cfb7
yoosple <yoosple93@gmail.com>
2023-01-13 11:42:49 +0900

    2023-01-13

--- vuejs/game-master/src/components/menus/DashboadPage.vue
+++ vuejs/game-master/src/components/menus/DashboadPage.vue
@@ -1,20 +1,30 @@
 <template>
-  <Game />
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
 </template>
 
 <script>
-import Game from './tic-tac-toe/Game'
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 export default {
-  name: 'App',
-  components: {
-    Game
+  name: 'BarChart',
+  components: { Bar },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
   }
 }
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap');
-body {
-  background-color: #252728;
-}
-</style>
